45554a8bb255fb4b2fadc87b118cde0639413eab
poohya1233 <117526492+poohya1233@users.noreply.github.com>
2025-01-04 15:19:21 +0700

    Add files via upload

---
+++ งานต้านบุหรี่/JS/page2.js
@@ -0,0 +1,32 @@
+let Imge2 = document.getElementById("imge2");
+let boutton2 = document.getElementById("buttom2");
+let Iwantsmoking2 = Iwantsmoking;
+
+function YES2(){
+    imge2.style.animationName = "OUT2";
+    imge2.style.animationDuration = "3s";
+    imge2.style.animationPlayState = "running";
+    boutton2.style.animationName = "OUT2";
+    boutton2.style.animationDuration = "3s";
+    boutton2.style.animationPlayState = "running";
+    setTimeout(() => {
+        window.location.href = '/HTMl/end3.html';
+    }, 1000);
+    
+    
+
+}
+
+
+function NO2(){
+    imge2.style.animationName = "OUT2";
+    imge2.style.animationDuration = "3s";
+    imge2.style.animationPlayState = "running";
+    boutton2.style.animationName = "OUT2";
+    boutton2.style.animationDuration = "3s";
+    boutton2.style.animationPlayState = "running";
+    setTimeout(() => {
+        window.location.href = '/HTMl/end2.html';
+    }, 1000);
+
+}
